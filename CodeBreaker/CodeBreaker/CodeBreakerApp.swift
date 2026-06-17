ccc46f754cb54cfbcfc4f4a5a4c8458558ceb714
Andres Paredes <andresparedesdesign@gmail.com>
2026-06-17 16:36:43 -0500

    New Project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Andres on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/CodeBreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by Andres on 17/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hola, soy Andrés")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
